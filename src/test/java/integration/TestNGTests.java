ee4e2756829656aac73327b372a0ed9576509ab4
Christian Stein <sormuras@gmail.com>
2019-01-25 11:05:19 +0100

    Add simple integration for debugging in IDEs...
    See #1

---
+++ src/test/java/integration/TestNGTests.java
@@ -0,0 +1,12 @@
+package integration;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestNGTests {
+
+  @Test
+  public void test() {
+    Assert.assertNull(null);
+  }
+}
